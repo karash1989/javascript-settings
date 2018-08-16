08ed142eb1d0faf6d6d510a082329706f40e2c8f
admin <admin@TeamCity>
2018-08-16 14:22:54 +0300

    TeamCity change in 'JavaScript' project: VCS roots of 'Reboot agent' build configuration were updated

--- .teamcity/patches/buildTypes/RebootAgent.kts
+++ .teamcity/patches/buildTypes/RebootAgent.kts
@@ -12,5 +12,9 @@ in the root project, and delete the patch script.
 create(DslContext.projectId, BuildType({
     id("RebootAgent")
     name = "Reboot agent"
+
+    vcs {
+        root(RelativeId("HttpsGithubComG0t4teamcityCourseCards"))
+    }
 }))
 
